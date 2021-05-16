64f85b8303e4fb81b256aa8abee3100cacecdc1f
김정근 <kjg.leh@gmail.com>
2021-05-16 11:46:32 +0900

    initialize project

---
+++ src/main/kotlin/me/kjgleh/springboot/batch/SpringbootBatchApplication.kt
@@ -0,0 +1,11 @@
+package me.kjgleh.springboot.batch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootBatchApplication>(*args)
+}
---
+++ src/test/kotlin/me/kjgleh/springboot/batch/SpringbootBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kjgleh.springboot.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
